53ff43c98aa550dd1cdf0f29d0cefec2333128b8
cowsys <cowsys@gmail.com>
2012-05-04 14:09:03 +0900

    fix

--- symlink.pl
+++ symlink.pl
@@ -16,8 +16,24 @@ chdir "$home$dirname";
 #    system 'git pull origin master';
 #}
 
-for (glob ".*") {
-    next if /\.(|git|svn)$/;
+my @dotifiles = (
+    '.bash_profile',
+    '.bashrc',
+    '.git',
+    '.gitconfig',
+    '.gitignore',
+    '.gitmodules',
+    '.gvimrc',
+    '.inputrc',
+    '.screenrc',
+    '.vim',
+    '.viminfo',
+    '.vimperator',
+    '.vimperatorrc',
+    '.vimrc',
+    '.zshrc',
+);
+for (@dotifiles) {
     system "rm -rf $home/$_" if -e "$home/$_";
     my $cmd = "ln -s $home/$dirname/$_ $home/$_";
     warn "created $home/$_";
